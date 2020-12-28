fefd70efd55e4a2875d6d310b017f6ee7624a619
Marcus Nilszén <39590503+mebn@users.noreply.github.com>
2020-12-28 14:47:13 +0100

    Initial Commit

---
+++ spotibar/spotibarApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotibarApp.swift
+//  spotibar
+//
+//  Created by Marcus Nilszén on 2020-12-28.
+//
+
+import SwiftUI
+
+@main
+struct spotibarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
