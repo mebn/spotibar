0307298da6f78a4202250b9e2aa9495f147e130f
Marcus Nilszén <39590503+mebn@users.noreply.github.com>
2020-12-28 15:49:04 +0100

    Prepped for menu bar app

--- spotibar/spotibarApp.swift
+++ spotibar/spotibarApp.swift
@@ -9,9 +9,28 @@ import SwiftUI
 
 @main
 struct spotibarApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
-        WindowGroup {
+        Settings {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var popover = NSPopover()
+    var statusBarItem: NSStatusItem?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        let view = NSHostingView(rootView: ContentView())
+        view.frame = NSRect(x: 0, y: 0, width: 200, height: 200)
+        let menuItem = NSMenuItem()
+        menuItem.view = view
+        let menu = NSMenu()
+        menu.addItem(menuItem)
+        
+        statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        statusBarItem?.button?.title = "SpotiBar"
+        statusBarItem?.menu = menu
     }
 }
